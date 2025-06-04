1fb0ec460497f875bba7876e514118401969e6a9
krishh26 <kishansinhparmar999@gmail.com>
2025-06-04 19:19:03 +0530

    changes done

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from 'src/environment/environment-prod';
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
   private apiUrl = '/search-ui/formdata';
-
-  constructor(private http: HttpClient) { }
+  baseUrl!: string;
+  constructor(
+    private http: HttpClient
+  ) {
+    this.baseUrl = environment.baseUrl;
+  }
 
   submitForm(formData: any): Observable<any> {
     const payload = {
