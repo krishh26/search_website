1a1b48241fac2846327cfa7ca7be1a94664a2b29
krishh26 <kishansinhparmar999@gmail.com>
2025-06-18 13:45:26 +0530

    changes done

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isHomePage: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Check initial route
+    this.checkIfHomePage();
+
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkIfHomePage();
+    });
+  }
+
+  private checkIfHomePage() {
+    const currentUrl = this.router.url;
+    this.isHomePage = currentUrl === '/' || currentUrl === '/home';
+  }
 }
