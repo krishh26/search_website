6592557b22bddc394435db7a99c9f4eb581a8a51
krishh26 <kishansinhparmar999@gmail.com>
2025-06-18 14:45:31 +0530

    chaanges done

--- src/app/module/home/home-page/home-page.component.ts
+++ src/app/module/home/home-page/home-page.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  selectedService: string = 'WorkAway'; // Default selection
+  showSkills: boolean = true; // Show skills by default for WorkAway
+  showServices: boolean = false; // Hide services by default
 
+  onServiceChange(event: any) {
+    this.selectedService = event.target.value;
+
+    if (this.selectedService === 'WorkAway') {
+      this.showSkills = true;
+      this.showServices = false;
+    } else if (this.selectedService === 'IT Subcontracting') {
+      this.showSkills = false;
+      this.showServices = true;
+    }
+  }
 }
